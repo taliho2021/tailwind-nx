496bc71bb5a420ef0b172f35f85062e861d6ef92
Tal Yo <tal@analink.com>
2022-12-19 08:44:04 -0500

    Initial commit

--- apps/app1/src/app/app.module.ts
+++ apps/app1/src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { Lib1Module } from '@tailwind-nx/lib1';
+import { Lib2Module } from '@tailwind-nx/lib2';
+import { NgModule } from '@angular/core';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, Lib1Module, Lib2Module],
   providers: [],
   bootstrap: [AppComponent],
 })
